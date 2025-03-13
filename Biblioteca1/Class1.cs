8934e0ff6bd5da9f58b6d8c116d97ec84198031c
mayraglez917 <mayraglez917@gmail.com>
2025-03-12 22:28:39 -0600

    Se agregaron los métodos Multiplicar y Dvidir

--- Biblioteca1/Class1.cs
+++ Biblioteca1/Class1.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Biblioteca1
 {
     public class Class1
-        //ingreso usuario Ariadna
-        // Ingreso usuario Mayra 
+    //ingreso usuario Ariadna
+    // Ingreso usuario Mayra 
     {
         public static double sumar(double uno, double dos)
         {
@@ -18,6 +18,23 @@ namespace Biblioteca1
         public static double restar(double uno, double dos)
         {
             return uno - dos;
+        }
+
+        public static double multiplicar(double uno, double dos)
+        {
+            return uno * dos;
+        }
+
+        public static double dividir(double uno, double dos)
+        {
+            if (dos != 0)
+            {
+                return uno / dos;
+            }
+            else
+            {
+                throw new DivideByZeroException("No se puede dividir entre cero.");
+            }
         }
     }
 }
