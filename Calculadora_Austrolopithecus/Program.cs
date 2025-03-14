e1f816bfcc77eed0ea0454b5223bf7d7305b0423
mayraglez917 <mayraglez917@gmail.com>
2025-03-14 15:31:44 -0600

    Menú agregado

--- Calculadora_Austrolopithecus/Program.cs
+++ Calculadora_Austrolopithecus/Program.cs
@@ -11,17 +11,63 @@ using Biblioteca1;
 
 namespace Calculadora_Austrolopithecus
 {
-    class Program
-    {//Usuario ESO TILÍN
-
-        private static double num1 = 0, num2 = 0, num0 = 0;
-        static void Main(string[] args)
+        class Program
         {
-            bool salir = false;
-            while (!salir)
+            private static double num1 = 0, num2 = 0, num0 = 0;
+            static void Main(string[] args)
             {
+                bool salir = false;
+                while (!salir)
+                {
+                    try
+                    {
+                        Console.WriteLine(" Menu Principal de Calculadora del Tecnologico de Tlaxiaco");
+                        Console.WriteLine("1. Sumar dos numeros");
+                        Console.WriteLine("2. Restar dos numeros");
+                        Console.WriteLine("3. Multiplicar");
+                        Console.WriteLine("4. Dividir");
+                        Console.WriteLine("5. Raiz Cuadrada");
+                        Console.WriteLine("6. Potencia");
+                        Console.WriteLine("7. Porcentaje");
+                        Console.WriteLine("8. Fraccion");
+                        Console.WriteLine("9. Salir");
+                        Console.WriteLine("------------------------------------------");
+                        Console.WriteLine("Elige una de las opciones");
+                        int opcion = Convert.ToInt32(Console.ReadLine());
+
+                        switch (opcion)
+                        {
+                            case 9:
+                                Console.WriteLine("Has elegido salir de la aplicación");
+                                Environment.Exit(1);
+                                salir = true;
+                                break;
+                            default:
+                                Console.WriteLine("Elige una opcion entre 1 y 9");
+                                break;
+                        }
+
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Error al ingresar!!");
+                    }
                 }
+            }
+
+            private static void teclado0()
+            {
+                Console.WriteLine("Introduzca un numero");
+                num0 = double.Parse(Console.ReadLine());
+            }
+            private static void teclado1()
+            {
+                Console.WriteLine("Introduzca el primer numero");
+                num1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Introduzca el segundo numero");
+                num2 = double.Parse(Console.ReadLine());
+            }
         }
+
     }
 
-}
