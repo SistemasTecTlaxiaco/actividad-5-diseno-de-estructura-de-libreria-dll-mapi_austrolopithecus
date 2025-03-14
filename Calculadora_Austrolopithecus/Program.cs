77d1c2d635b98a39084ac9b60061cef77b1aac91
ariad <23620030@tlaxiaco.tecnm.mx>
2025-03-13 18:00:53 -0600

    Agregue la referencia y el menu de opciones para el usuario

--- Calculadora_Austrolopithecus/Program.cs
+++ Calculadora_Austrolopithecus/Program.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Biblioteca1;
+
+//ingreso de usuario Ariadna 
+//ingreso de usuario Paola
+// Ingreso de Usuario Mayra
 
 namespace Calculadora_Austrolopithecus
 {
-    internal class Program
+    class Program
     {
+
+        private static double num1 = 0, num2 = 0, num0 = 0;
         static void Main(string[] args)
-        //ingreso de usuario Ariadna 
-        //ingreso de usuario Paola
         {
-            // Ingreso de Usuario Mayra 
+            bool salir = false;
+            while (!salir)
+            {
+                try
+                {
+                    Console.WriteLine(" Menu Principal de Calculadora del Tecnologico de Tlaxiaco");
+                    Console.WriteLine("1. Sumar dos numeros");
+                    Console.WriteLine("2. Restar dos numeros");
+                    Console.WriteLine("3. Multiplicar");
+                    Console.WriteLine("4. Dividir");
+                    Console.WriteLine("5. Raiz Cuadrada");
+                    Console.WriteLine("6. Potencia");
+                    Console.WriteLine("7. Porcentaje");
+                    Console.WriteLine("8. Fraccion");
+                    Console.WriteLine("9. Salir");
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("Elige una de las opciones");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                
+                
+                }
         }
     }
 }
